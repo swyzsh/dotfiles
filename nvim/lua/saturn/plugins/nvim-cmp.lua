9bf5e9c6ee1753c7603fb86754da1e64029c988e
Swyzsh <swyzsh@protonmail.com>
2025-04-19 05:03:47 +0530

    update neovim

--- nvim/lua/saturn/plugins/nvim-cmp.lua
+++ nvim/lua/saturn/plugins/nvim-cmp.lua
@@ -2,6 +2,7 @@ return {
 	"hrsh7th/nvim-cmp",
 	event = "InsertEnter", -- for use in only insert mode
 	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-buffer", -- source for text in buffer
 		"hrsh7th/cmp-path", -- source for file system paths
 		{
@@ -11,7 +12,7 @@ return {
 			-- install jsregexp (optional!).
 			build = "make install_jsregexp",
 		},
-		"saadparwaiz1/cmp_luasnip", -- for autocompletion
+		"saadparwaiz1/cmp_luasnip",
 		"rafamadriz/friendly-snippets", -- useful snippets
 		"onsails/lspkind.nvim", -- vs-code like pictograms
 	},
